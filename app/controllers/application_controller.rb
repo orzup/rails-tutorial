06e6bbcd2d47ce8a5434ffe3dffe527fba5639f3
Asami Nakano <nakano.a@pepabo.com>
2015-06-10 11:54:10 +0900

    show "hola, mundo!"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render text: "hello, world!"
+    render text: "hola, mundo!"
   end
 end
