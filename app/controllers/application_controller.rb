a72e379d5f9fe90aa89315ae076a916e3ed2d94b
Asami Nakano <nakano.a@pepabo.com>
2015-06-10 11:55:49 +0900

    add non-ASCII characters to "hola,mundo!"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render text: "hola, mundo!"
+    render text: "¡Hola, mundo!"
   end
 end
