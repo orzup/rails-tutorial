a03534358a68e4fa3d835a7e88912a235e71ebbc
Asami Nakano <nakano.a@pepabo.com>
2015-06-10 15:23:53 +0900

    Finish toy_app!

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150610051511) do
+ActiveRecord::Schema.define(version: 20150610060259) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
