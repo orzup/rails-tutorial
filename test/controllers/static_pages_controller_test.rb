f3478e5a91959a250fae4c5525b1af5cc41b6a91
Asami Nakano <nakano.a@pepabo.com>
2015-06-11 15:40:16 +0900

    static_pages_controller_test.rb内の各タイトルのテストを修正
    共通部分が直打ちしてあったので、共通部分をsetupメソッドに抽出した。

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,26 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "ホーム | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "ホーム | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "ヘルプ | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "ヘルプ | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "アバウト | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "アバウト | #{@base_title}"
   end
 end
