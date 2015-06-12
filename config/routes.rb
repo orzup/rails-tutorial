75acf690cdb1ac0f4aa8d424a9dac5dec5cc00c6
Asami Nakano <nakano.a@pepabo.com>
2015-06-12 16:53:03 +0900

    ヘッダーとフッターのリンクを接続

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  get 'help' => 'static_pages#help'
+  get 'about' => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
