d887e53bf4cf52070f9ce1170efe20d6b57ea464
Asami Nakano <nakano.a@pepabo.com>
2015-06-12 17:02:27 +0900

    レイアウトとroutesの調整完了

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
+  get 'signup' => 'users#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
