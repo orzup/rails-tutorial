a4270ddda648b0f6bc5a03de4405f8041f1092a5
Asami Nakano <nakano.a@pepabo.com>
2015-06-15 13:14:12 +0900

    結合テストをリファクタリング
    full_titleメソッドを使用し共通部分を抽出した。

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,10 +1,6 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  setup do
-    @base_title = "Ruby on Rails Tutorial のサンプルアプリ"
-  end
-
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -13,6 +9,6 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     get signup_path
-    assert_select "title", "サインアップ | #{@base_title}"
+    assert_select "title", full_title("サインアップ")
   end
 end
