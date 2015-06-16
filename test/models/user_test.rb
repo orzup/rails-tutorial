72aa7e787ea3369565a47f4ddd8603096bea962c
Asami Nakano <nakano.a@pepabo.com>
2015-06-16 11:48:37 +0900

    emailのドメイン名にドットが連続するものをはじくように修正

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -31,7 +31,9 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "email validation should accept valid addresses" do
-    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses = %w[user@example,com user_at_foo.org 
+      user.name@example.foo@bar_baz.com foo@bar+baz.com
+      foo@bar..com]
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
