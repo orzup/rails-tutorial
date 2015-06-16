71e8aea349075d57c3335c5a4bb48e204ea39cfa
Asami Nakano <nakano.a@pepabo.com>
2015-06-16 15:46:10 +0900

    サインアップ機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,21 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "サンプルアプリへようこそ！"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
