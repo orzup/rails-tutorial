af1f11b8c3b4de87bec9e7115592f2e3cc41968d
Asami Nakano <nakano.a@pepabo.com>
2015-06-17 16:12:58 +0900

    ログアウト機能を追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,5 +14,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out
+    redirect_to root_url
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get 'signup' => 'users#new'
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
-  get 'logout' => 'sessions#destroy'
+  delete 'logout' => 'sessions#destroy'
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
