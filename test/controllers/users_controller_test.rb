c898a377daa9ca0c2060871ed65354ddf3444d58
Asami Nakano <nakano.a@pepabo.com>
2015-06-22 12:14:05 +0900

    ユーザー一覧を表示する機能を追加

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,6 +6,11 @@ class UsersControllerTest < ActionController::TestCase
     @other_user = users(:archer)
   end
 
+  test "should redirect" do
+    get :index
+    assert_redirected_to login_url
+  end
+
   test "should get new" do
     get :new
     assert_response :success
