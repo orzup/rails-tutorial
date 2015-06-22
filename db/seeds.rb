40aa2c6137fedc4e3f5ee2e9c39b8b8107cc71bc
Asami Nakano <nakano.a@pepabo.com>
2015-06-22 15:25:54 +0900

    ユーザーに管理権限の付与と削除機能を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,8 @@ User.create!(
   name:  "Example User",
   email: "example@railstutorial.org",
   password: "foobar",
-  password_confirmation: "foobar"
+  password_confirmation: "foobar",
+  admin: true
 )
 
 99.times do |n|
