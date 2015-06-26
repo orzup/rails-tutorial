d30d1645fa5fe8b263a67fcc7c25847a984a4952
Asami Nakano <nakano.a@pepabo.com>
2015-06-26 17:00:24 +0900

    仮登録のユーザーがユーザーページに行くとhomeにリダイレクトされるか確認するテストを追加

---
+++ test/integration/users_show_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class UsersShowTest < ActionDispatch::IntegrationTest
+  def setup
+    @activated = users(:michael)
+    @non_activated = users(:newuser)
+  end
+
+  test "show as activated user" do
+    log_in_as(@activated)
+    get user_path(@activated)
+    assert_template 'users/show'
+  end
+
+  test "show as non-activated user" do
+    log_in_as(@non_activated)
+    get user_path(@non_activated)
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_template 'static_pages/home'
+  end
+end
