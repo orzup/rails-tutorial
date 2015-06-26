dc3a4784a45bef55113cadb8160dcba2d0250599
Asami Nakano <nakano.a@pepabo.com>
2015-06-26 17:38:40 +0900

    絶対に通るテストを削除

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -27,23 +27,6 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     end
   end
 
-  test "index unincluding non-activated" do
-    log_in_as(@admin)
-    get users_path
-    assert_template 'users/index'
-    assert_select 'div.pagination'
-    total_pages = User.paginate(page:1).total_pages
-    (1..total_pages).each do |i|
-      page_of_users = User.paginate(page: i)
-      page_of_users.each do |user|
-        unless user.activated?
-          assert_not user.activated?
-          assert_nil user.activated_at
-        end
-      end
-    end
-  end
-
   test "index as non-admin" do
     log_in_as(@non_admin)
     get users_path
