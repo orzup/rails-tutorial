6b452df713acd45da5185979981327e7ecee7733
Asami Nakano <nakano.a@pepabo.com>
2015-06-26 16:29:18 +0900

    emailのアクティベートが完了したユーザーのみを表示するように変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,13 @@ class UsersController < ApplicationController
   before_action :admin_user, only: :destroy
 
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.where(activated:true).paginate(page: params[:page])
+    p @users.count
   end
 
   def show
     @user = User.find(params[:id])
+    redirect_to root_url and return unless @user.activated?
   end
 
   def new
--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -13,13 +13,34 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     assert_select 'div.pagination'
     first_page_of_users = User.paginate(page: 1)
     first_page_of_users.each do |user|
-      assert_select 'a[href=?]', user_path(user), text: user.name
-      unless user == @admin
-        assert_select 'a[href=?]', user_path(user), text: 'delete'
+      if user.activated?
+        assert_select 'a[href=?]', user_path(user), text: user.name
+        unless user == @admin
+          assert_select 'a[href=?]', user_path(user), text: 'delete'
+        end
+      else
+        assert_select 'a[href=?]', user_path(user), text: user.name, count: 0
       end
     end
     assert_difference 'User.count', -1 do
       delete user_path(@non_admin)
+    end
+  end
+
+  test "index unincluding non-activated" do
+    log_in_as(@admin)
+    get users_path
+    assert_template 'users/index'
+    assert_select 'div.pagination'
+    total_pages = User.paginate(page:1).total_pages
+    (1..total_pages).each do |i|
+      page_of_users = User.paginate(page: i)
+      page_of_users.each do |user|
+        unless user.activated?
+          assert_not user.activated?
+          assert_nil user.activated_at
+        end
+      end
     end
   end
 
