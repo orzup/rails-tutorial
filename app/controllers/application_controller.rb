d95cffa8e1c5d1a63396d328c808be9f4dd20842
Asami Nakano <nakano.a@pepabo.com>
2015-06-29 16:47:45 +0900

    マイクロポストの一覧表示、投稿、削除機能を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  private
+
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "ログインしてください"
+        redirect_to login_url
+      end
+    end
 end
