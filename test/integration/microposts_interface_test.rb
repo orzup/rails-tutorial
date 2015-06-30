dc155bc193c7be208ce0956beef6883617b5e137
Asami Nakano <nakano.a@pepabo.com>
2015-06-30 15:57:28 +0900

    サイドバーのmicropostの数が正しいかを確認するテストを追加

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -32,4 +32,18 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     get user_path(users(:archer))
     assert_select 'a', text: '削除', count: 0
   end
+
+  test "micropost sidebar count" do
+    log_in_as(@user)
+    get root_path
+    assert_match "#{@user.feed.count} microposts", response.body
+    # User with zero microposts
+    other_user = users(:mallory)
+    log_in_as(other_user)
+    get root_path
+    assert_match "0 microposts", response.body
+    other_user.microposts.create!(content: "A micropost")
+    get root_path
+    assert_match "#{other_user.feed.count} micropost", response.body
+  end
 end
