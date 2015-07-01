138d6c44d64d77532c1276edfc83e92fd6715d39
Asami Nakano <nakano.a@pepabo.com>
2015-07-01 13:19:53 +0900

    フォロー、フォロワーの関係を追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -75,4 +75,15 @@ class UserTest < ActiveSupport::TestCase
       @user.destroy
     end
   end
+
+  test "should follow and unfollow a user" do
+    michael = users(:michael)
+    archer = users(:archer)
+    assert_not michael.following?(archer)
+    michael.follow(archer)
+    assert michael.following?(archer)
+    assert archer.followers.include?(michael)
+    michael.unfollow(archer)
+    assert_not michael.following?(archer)
+  end
 end
