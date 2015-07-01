d53e7460c7853c83adf744065576b27acbf6a4b1
Asami Nakano <nakano.a@pepabo.com>
2015-07-01 16:29:00 +0900

    フィード機能を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -65,7 +65,9 @@ class User < ActiveRecord::Base
   end
 
   def feed
-    Micropost.where("user_id = ? ", id)
+    following_ids = "SELECT followed_id FROM relationships
+                 WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id:id)
   end
 
   def follow(other_user)
