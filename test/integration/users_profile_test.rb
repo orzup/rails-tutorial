9ffdea0edd09412e4cd97098264ba9cc7c84f9a8
Asami Nakano <nakano.a@pepabo.com>
2015-07-02 11:39:47 +0900

    プロフィール画面のインテグレーションテストを追加

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -6,17 +6,35 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
   end
-  # 
-  # test "profile display" do
-  #   get user_path(@user)
-  #   assert_template 'users/show'
-  #   assert_select 'title', full_title(@user.name)
-  #   assert_select 'h1', text: @user.name
-  #   assert_select 'h1>imag.gravatar'
-  #   assert_match @user.microposts.count.to_s, response.body
-  #   assert_select 'div.pagination'
-  #   @user.microposts.paginate(page: 1).each do |micropost|
-  #     assert_match micropost.content, response.body
-  #   end
-  # end
+
+  test "profile display as not logged in" do
+    get user_path(@user)
+    assert_template 'users/show'
+    assert_select 'title', full_title(@user.name)
+    assert_select 'img.gravatar'
+    assert_select 'h1', text: @user.name
+    assert_match @user.microposts.count.to_s, response.body
+    assert_match @user.following.count.to_s, response.body
+    assert_match @user.followers.count.to_s, response.body
+    assert_select 'div.pagination'
+    @user.microposts.paginate(page: 1).each do |micropost|
+      assert_match micropost.content, response.body
+    end
+  end
+
+  test "profile display as logged in" do
+    log_in_as(@user)
+    get user_path(@user)
+    assert_template 'users/show'
+    assert_select 'title', full_title(@user.name)
+    assert_select 'img.gravatar'
+    assert_select 'h1', text: @user.name
+    assert_match @user.microposts.count.to_s, response.body
+    assert_match @user.following.count.to_s, response.body
+    assert_match @user.followers.count.to_s, response.body
+    assert_select 'div.pagination'
+    @user.microposts.paginate(page: 1).each do |micropost|
+      assert_match micropost.content, response.body
+    end
+  end
 end
